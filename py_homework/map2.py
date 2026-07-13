db49e2d9dc7cd768a029016936ab32219f4c29d2
wangxinpingsan-ux <wangxinpingsan@gmail.com>
2026-07-14 00:30:08 +0800

    1

---
+++ py_homework/map2.py
@@ -0,0 +1,23 @@
+#str_list = ["1", "2", "3", "4"]
+
+
+#result = map(int, str_list)
+
+
+#print(list(result))
+
+str1="lucas"
+print(len(str1))
+
+str2=[1,2,5]
+print(len(str2))
+
+num=721
+
+str3=str(num)
+
+
+
+print(f"個位={str3[0]}")
+print(f"十位={str3[1]}")
+print(f"百位={str3[2]}")
